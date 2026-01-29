44e4cd12dab306f815485a5cc7ed82019ec22bf2
Tiago <tiagosune1@hotmail.com>
2026-01-29 03:17:02 -0300

    feat: Configura segurança com desabilitação do CSRF e filtro de autenticação

---
+++ qrart/qrart/src/main/java/com/tiagosune/qrcode/qrart/security/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.tiagosune.qrcode.qrart.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll() // libera register/login
+                        .anyRequest().authenticated()               // o resto continua protegido (por enquanto)
+                );
+
+        return http.build();
+    }
+}
