8e446821612b8fa23e35ba686685aa7d1c155840
Tiago <tiagosune1@hotmail.com>
2026-01-29 03:16:21 -0300

    feat: Adiciona serviço JwtService e dependências do JJWT no projeto

---
+++ qrart/qrart/src/main/java/com/tiagosune/qrcode/qrart/security/JwtService.java
@@ -0,0 +1,43 @@
+package com.tiagosune.qrcode.qrart.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtService {
+
+    public String generateToken(String email) {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + 1000 * 60 * 60);
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(now)
+                .expiration(expiration)
+                .signWith(getSigningKey())
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .verifyWith((SecretKey) getSigningKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    public boolean isTokenValid(String token, String email) {
+        return extractUsername(token).equals(email);
+    }
+
+    private Key getSigningKey() {
+        String secretKey = "sua-chave-super-secreta-com-pelo-menos-32-bytes!";
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
