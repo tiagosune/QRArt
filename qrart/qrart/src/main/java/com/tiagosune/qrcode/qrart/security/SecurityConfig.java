accf746248372879763c38f595dd485aac256ae0
Tiago <tiagosune1@hotmail.com>
2026-01-29 23:28:38 -0300

    feat: Adiciona JwtAuthenticationFilter à cadeia de filtros na configuração de segurança

--- qrart/qrart/src/main/java/com/tiagosune/qrcode/qrart/security/SecurityConfig.java
+++ qrart/qrart/src/main/java/com/tiagosune/qrcode/qrart/security/SecurityConfig.java
@@ -1,21 +1,27 @@
 package com.tiagosune.qrcode.qrart.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**").permitAll() // libera register/login
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()               // o resto continua protegido (por enquanto)
-                );
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
