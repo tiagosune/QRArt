68abe63cbb1ed3f068928a57c5dedf80182b4539
Tiago <tiagosune1@hotmail.com>
2026-01-29 23:56:27 -0300

    feat: Adiciona endpoint `/api/users/me` para retornar dados do usuário autenticado

--- qrart/qrart/src/main/java/com/tiagosune/qrcode/qrart/controller/UsersController.java
+++ qrart/qrart/src/main/java/com/tiagosune/qrcode/qrart/controller/UsersController.java
@@ -1,8 +1,32 @@
 package com.tiagosune.qrcode.qrart.controller;
 
 
+import com.tiagosune.qrcode.qrart.dto.UsersResponse;
+import com.tiagosune.qrcode.qrart.model.Users;
+import com.tiagosune.qrcode.qrart.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
+@RequestMapping("/api/users")
 public class UsersController {
+
+    private final UsersRepository usersRepository;
+
+    @GetMapping("/me")
+    public UsersResponse me(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Users user = (Users) auth.getPrincipal();
+        UsersResponse response = new UsersResponse();
+        response.setId(user.getId());
+        response.setName(user.getName());
+        response.setEmail(user.getEmail());
+
+        return response;
+    }
 }
---
+++ qrart/qrart/src/main/java/com/tiagosune/qrcode/qrart/dto/UsersResponse.java
@@ -0,0 +1,12 @@
+package com.tiagosune.qrcode.qrart.dto;
+
+import lombok.Data;
+
+@Data
+public class UsersResponse {
+
+    private Long id;
+    private String name;
+    private String email;
+
+}
