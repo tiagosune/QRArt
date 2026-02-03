d877cf706a19b986fb0516a21973962d7103cf8b
Tiago <tiagosune1@hotmail.com>
2026-02-03 17:52:56 -0300

    refactor: Remove classe `CreateQRCodeRequest` e referências não utilizadas no QrCodeController

--- qrart/qrart/src/main/java/com/tiagosune/qrcode/qrart/controller/QrCodeController.java
+++ qrart/qrart/src/main/java/com/tiagosune/qrcode/qrart/controller/QrCodeController.java
@@ -11,7 +11,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
-import com.tiagosune.qrcode.qrart.dto.CreateQRCodeRequest;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.ArrayList;
